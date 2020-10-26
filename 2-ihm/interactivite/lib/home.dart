e3091b5dddfb2fc7cfe2bbc5f5dcfd68255826be
Maxime Britto <maxime@purplegiraffe.fr>
2020-10-26 08:15:31 +0100

    22_3_Utiliser_le_Widget_IconButton

--- 2-ihm/interactivite/lib/home.dart
+++ 2-ihm/interactivite/lib/home.dart
@@ -6,8 +6,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  _plusButtonClicked() {
+    print("Bazinga!");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          IconButton(
+              icon: Icon(Icons.plus_one),
+              iconSize: 50,
+              onPressed: _plusButtonClicked)
+        ],
+      ),
+    );
   }
 }
