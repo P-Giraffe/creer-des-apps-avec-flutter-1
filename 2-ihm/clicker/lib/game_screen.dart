6b922914f950806e6216d256e69abbd26e2cb5d2
Maxime Britto <maxime@purplegiraffe.fr>
2020-11-05 18:11:24 +0100

    25_1_Ameliorer_la_mise_en_page

--- 2-ihm/clicker/lib/game_screen.dart
+++ 2-ihm/clicker/lib/game_screen.dart
@@ -9,25 +9,39 @@ class _GameScreenState extends State<GameScreen> {
   var _clickCount = 0;
   var _isCounting = false;
 
-  _startCounting() {}
+  _startCounting() {
+    setState(() {
+      _isCounting = true;
+    });
+  }
 
-  _clickButtonTouched() {}
+  _clickButtonTouched() {
+    setState(() {
+      _clickCount++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Clicker"),
       ),
-      body: Column(
-        children: [
-          Text("Nombre de clics : $_clickCount"),
-          if (_isCounting == false)
-            ElevatedButton(
-                onPressed: _startCounting, child: Text("Commencer à compter")),
-          if (_isCounting)
-            IconButton(
-                icon: Icon(Icons.plus_one), onPressed: _clickButtonTouched)
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("Nombre de clics : $_clickCount"),
+            if (_isCounting)
+              IconButton(
+                  icon: Icon(Icons.plus_one), onPressed: _clickButtonTouched),
+            Spacer(),
+            if (_isCounting == false)
+              ElevatedButton(
+                  onPressed: _startCounting,
+                  child: Text("Commencer à compter")),
+          ],
+        ),
       ),
     );
   }
