e9f28c056a85bd0d458e6b65cace81dee33878c0
Maxime Britto <maxime@purplegiraffe.fr>
2021-01-06 17:44:26 +0100

    52_6_Charger_et_afficher_du_texte_avec_Flutter

--- 4-async-data/exemple_telechargement/lib/home.dart
+++ 4-async-data/exemple_telechargement/lib/home.dart
@@ -7,14 +7,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Future<String> _chargerDonnees() async {
+    final response =
+        await http.get("https://jsonplaceholder.typicode.com/users/1");
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception("Erreur de chargement des données");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(child: FutureBuilder(
+      body: Center(
+          child: FutureBuilder(
+        future: _chargerDonnees(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text("Données chargées");
+            return Text(snapshot.data);
           } else if (snapshot.hasError) {
             return Text("Erreur de chargement");
           } else {
