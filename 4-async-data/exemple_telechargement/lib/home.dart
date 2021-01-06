7d0d0fcea021b98b289e0865c70611fe61a6bbef
Maxime Britto <maxime@purplegiraffe.fr>
2021-01-06 17:32:33 +0100

    preparation 52_6_Charger_et_afficher_du_texte_avec_Flutter

--- 4-async-data/exemple_telechargement/lib/home.dart
+++ 4-async-data/exemple_telechargement/lib/home.dart
@@ -11,7 +11,17 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(child: Text("Utiliser des Webservices HTTP avec Flutter")),
+      body: Center(child: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text("Données chargées");
+          } else if (snapshot.hasError) {
+            return Text("Erreur de chargement");
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      )),
     );
   }
 }
