d1dce57f7aafff00ed9cccd2f6039e62678fd266
Maxime Britto <maxime@purplegiraffe.fr>
2021-01-06 17:23:43 +0100

    52_5_Installer_un_package_en_Dart__HTTP

---
+++ 4-async-data/exemple_telechargement/lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text("Utiliser des Webservices HTTP avec Flutter")),
+    );
+  }
+}
