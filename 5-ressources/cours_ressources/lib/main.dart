5caf6dc25ac60fcc657382acd775264006f6cb6b
Maxime Britto <maxime@purplegiraffe.fr>
2021-01-22 11:12:54 +0100

    51_3_Gerer_le_mode_sombre

--- 5-ressources/cours_ressources/lib/main.dart
+++ 5-ressources/cours_ressources/lib/main.dart
@@ -20,7 +20,12 @@ class MyApp extends StatelessWidget {
           // or simply save your changes to "hot reload" in a Flutter IDE).
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
-          primarySwatch: Colors.blue),
+          primarySwatch: Colors.orange,
+          textTheme: TextTheme(bodyText1: TextStyle()),
+          brightness: Brightness.light),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
