5a323dd499d270cfca3b464b855e8de9300fc7f9
Maxime Britto <maxime@purplegiraffe.fr>
2021-03-16 12:08:30 +0100

    62_5_Rendre_les_objets_GamesManager_intelligents

--- 6-architecture/clicker_liste_scores/lib/Model/games_manager.dart
+++ 6-architecture/clicker_liste_scores/lib/Model/games_manager.dart
@@ -4,6 +4,22 @@ class GamesManager {
   Game? _currentGame;
   final List<Game> _previousGames = [];
 
+  Game? get currentGame => _currentGame;
+  Game? get bestGame {
+    Game? topGame;
+    for (final game in _previousGames) {
+      if (topGame == null || topGame.score < game.score) {
+        topGame = game;
+      }
+    }
+    return topGame;
+  }
+
+  bool get isGameInProgress {
+    final game = _currentGame;
+    return game != null && game.isInProgress;
+  }
+
   startNewGame({required String userName}) {
     final newGame = Game(playerName: userName);
     newGame.start();
