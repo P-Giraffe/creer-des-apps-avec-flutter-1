f86496259145efb22ec95ebbf5816bc1c066faaf
Maxime Britto <maxime@purplegiraffe.fr>
2021-04-08 19:08:58 +0200

    72_5_Preparer_Floor_et_lentite_SQL

--- 7-storage/clicker_sauvegarde/lib/interfaces/games_local_data_manager.dart
+++ 7-storage/clicker_sauvegarde/lib/interfaces/games_local_data_manager.dart
@@ -1,4 +1,5 @@
 import 'package:clicker/Model/game.dart';
+import 'package:floor/floor.dart';
 
 class GamesLocalDataManager {
   Future<void> addNewGame(Game game) async {}
@@ -6,4 +7,20 @@ class GamesLocalDataManager {
   Future<List<Game>> getGamesList() async {
     return [];
   }
+}
+
+@entity
+class FLGame {
+  @PrimaryKey(autoGenerate: true)
+  final int id;
+  final String playerName;
+  final int score;
+
+  FLGame(this.id, this.playerName, this.score);
+
+  FLGame.fromGame(Game game)
+      : this.playerName = game.playerName,
+        this.score = game.score;
+
+  Game get game => Game(playerName: playerName, score: score);
 }
