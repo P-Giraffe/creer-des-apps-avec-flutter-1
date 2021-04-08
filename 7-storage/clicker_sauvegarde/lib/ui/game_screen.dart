91445f74fd570342c2db0e636ff311134623c5bd
Maxime Britto <maxime@purplegiraffe.fr>
2021-04-08 18:52:01 +0200

    72_4_Preparer_le_GameScreen_pour_quil_utilise_la_base_de_donnees

--- 7-storage/clicker_sauvegarde/lib/ui/game_screen.dart
+++ 7-storage/clicker_sauvegarde/lib/ui/game_screen.dart
@@ -1,20 +1,52 @@
 import 'dart:async';
 
-import 'package:clicker/Model/game.dart';
 import 'package:clicker/Model/games_manager.dart';
 import 'package:clicker/generated/l10n.dart';
 import 'package:clicker/ui/hall_of_fame_screen.dart';
 import 'package:flutter/material.dart';
 
-class GameScreen extends StatefulWidget {
+class GameScreen extends StatelessWidget {
+  final gamesManager = GamesManager();
+
   @override
-  _GameScreenState createState() => _GameScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).app_name),
+      ),
+      body: SafeArea(
+        child: FutureBuilder(
+          future: gamesManager.loadGamesListFromLocalData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData || snapshot.hasError) {
+              return _GameScreenContent(
+                gamesManager: gamesManager,
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+    );
+  }
 }
 
-class _GameScreenState extends State<GameScreen> {
-  final gamesManager = GamesManager();
+class _GameScreenContent extends StatefulWidget {
+  final GamesManager gamesManager;
+  _GameScreenContent({Key? key, required this.gamesManager}) : super(key: key);
+
+  @override
+  __GameScreenContentState createState() =>
+      __GameScreenContentState(gamesManager);
+}
+
+class __GameScreenContentState extends State<_GameScreenContent> {
+  final GamesManager gamesManager;
   var _currentPlayerName = "";
   var _currentNameFieldController = TextEditingController();
+
+  __GameScreenContentState(this.gamesManager);
 
   _startCounting() {
     setState(() {
@@ -52,45 +84,37 @@ class _GameScreenState extends State<GameScreen> {
     final bestGame = gamesManager.bestGame;
     final currentGame = gamesManager.currentGame;
     bool isGameInProgress = gamesManager.isGameInProgress;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).app_name),
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            if (isGameInProgress == false)
-              TextField(
-                  autocorrect: false,
-                  onChanged: _currentUsernameChanged,
-                  controller: _currentNameFieldController),
-            if (bestGame != null)
-              Text(S.current.point_record(bestGame.playerName, bestGame.score)),
-            if (currentGame != null)
-              Text(S.current.click_count(currentGame.score))
-            else
-              Text(S.of(context).before_game_text),
-            if (isGameInProgress)
-              IconButton(
-                  icon: Icon(Icons.plus_one), onPressed: _clickButtonTouched)
-            else
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) =>
-                          HallOfFameScreen(gameList: gamesManager.bestGameList),
-                    ));
-                  },
-                  child: Text(S.of(context).hall_of_fame)),
-            Spacer(),
-            if (isGameInProgress == false)
-              ElevatedButton(
-                  onPressed: _startCounting,
-                  child: Text(S.of(context).game_start_button)),
-          ],
-        ),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (isGameInProgress == false)
+          TextField(
+              autocorrect: false,
+              onChanged: _currentUsernameChanged,
+              controller: _currentNameFieldController),
+        if (bestGame != null)
+          Text(S.current.point_record(bestGame.playerName, bestGame.score)),
+        if (currentGame != null)
+          Text(S.current.click_count(currentGame.score))
+        else
+          Text(S.of(context).before_game_text),
+        if (isGameInProgress)
+          IconButton(icon: Icon(Icons.plus_one), onPressed: _clickButtonTouched)
+        else
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      HallOfFameScreen(gameList: gamesManager.bestGameList),
+                ));
+              },
+              child: Text(S.of(context).hall_of_fame)),
+        Spacer(),
+        if (isGameInProgress == false)
+          ElevatedButton(
+              onPressed: _startCounting,
+              child: Text(S.of(context).game_start_button)),
+      ],
     );
   }
 }
